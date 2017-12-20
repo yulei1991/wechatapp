2e897fec678c3bf2ca8cf8ad9ae3ffeac4c78ebd
yulei <136950242@qq.com>
2017-12-20 22:05:09 +0800

    init

---
+++ pages/tips/tips.js
@@ -0,0 +1,89 @@
+// pages/tips/tips.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    this.setData({
+      tips: ['iseedeadpeople 地图全开',
+        'allyourbasearebelongtous 立即获胜',
+        'somebodysetupusthebomb 立即失败',
+        'thereisnospoon 无限魔法',
+        'whosyourdaddy 无敌模式',
+        'strengthandhonor 在胜利或者失败了之后继续玩',
+        'warpten 快速建设',
+        'keyseroze 增加500金钱',
+        'iocainepowder 快速灭亡',
+        'leafittome 增加500木材',
+        'pointbreak 获得食物',
+        'thedudeabides 安静下来',
+        'itvexesme 拒绝胜利',
+        'whoisjohngalt 研究',
+        'synergy 科技树全开',
+        'sharpandshiny 升级',
+        'riseandshine 设置早上时间',
+        'lightsout 设置晚上时间',
+        'daylightsavings (hour) 设置一天的指定时刻',
+        'daylightsavings 白昼连续开关',
+        'motherland (race) (level) 选关',
+        'whosyourdaddy 无敌']
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
