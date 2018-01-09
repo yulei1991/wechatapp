5cbde37c03f3c6853e501204ee9320bafeabb700
yulei <136950242@qq.com>
2018-01-09 23:46:59 +0800

    完善打牌页面

--- pages/poker/poker.js
+++ pages/poker/poker.js
@@ -5,14 +5,17 @@ Page({
    * 页面的初始数据
    */
   data: {
-    point: 0
+    point: 0,
+    cardPoints: [1,2,3,5],
+    selectedCard: 0,
+    voteSuccess: false
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+
   },
 
   /**
@@ -62,5 +65,63 @@ Page({
    */
   onShareAppMessage: function () {
   
+  },
+
+  clickCard: function(e){
+    let currentPoint = e.currentTarget.dataset.value;
+    if(this.data.selectedCard==currentPoint){
+      this.setData({
+        selectedCard: 0
+      })
+    }else{
+      this.setData({
+        point: currentPoint,
+        selectedCard: currentPoint
+      })
+    }
+  },
+  submit:function(e){
+    let finalPoint = this.data.selectedCard;
+    if (finalPoint==0){
+      wx.showModal({
+        title: '提示',
+        content: '请选择一张卡片',
+        showCancel: false,
+        success: function (res) {
+          if (res.confirm) {
+            // console.log('用户点击确定')
+          } else if (res.cancel) {
+            // console.log('用户点击取消')
+          }
+        }
+      })
+    }else{
+      this.vote(finalPoint);
+    }
+  },
+  vote: function(point){
+    let $this = this;
+    wx.request({
+      url: 'https://m.ctrip.com/restapi/soa2/10934/hotel/customer/getmarquees',
+      data: { "alliance": { "ishybrid": 0 }, "head": { "cid": point, "ctok": "", "cver": "1.0", "lang": "01", "sid": "8888", "syscode": "09", "auth": null}, "contentType": "json" },
+      method: 'POST',
+      success: function (res) {
+        $this.data.voteSuccess = true;
+      },
+      complete: function(res){
+        wx.showModal({
+          title: '提示',
+          content: $this.data.voteSuccess ? '出牌成功，点数为' + point : '出牌失败，请重试',
+          showCancel: false,
+          success: function (res) {
+            if (res.confirm) {
+              // console.log('用户点击确定')
+            } else if (res.cancel) {
+              // console.log('用户点击取消')
+            }
+          }
+        })
+      }
+    })
   }
 })
